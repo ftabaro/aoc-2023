f31120383d361f4a982779d072b039263fc6ff6b
Francesco Tabaro <francesco.tabaro@embl.it>
2023-12-06 16:36:57 +0100

    black

--- day6/solution1.py
+++ day6/solution1.py
@@ -1,17 +1,19 @@
 import re
 import math
 
+
 def solve(time: int, distance: int) -> int:
     time_press1 = math.floor((time - math.sqrt(time * time - 4 * distance)) / 2)
     time_press2 = math.ceil((time + math.sqrt(time * time - 4 * distance)) / 2)
 
     return len(list(range(time_press1 + 1, time_press2)))
 
+
 def main(path: str) -> int:
     """
     Day 6 solution to part 1
     """
-    
+
     with open(path, encoding="utf-8") as file_handler:
         lines = file_handler.readlines()
 
